5c1132d04243ec3dffd39f2f133d0d664d916512
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-02-23 16:48:49 +0300

    Initial commit

---
+++ src/main/java/com/mehmetsukrukavak/SpringOAuthDemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.mehmetsukrukavak.SpringOAuthDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("greet")
+    public String greet() {
+        return "Hello World!";
+    }
+}
