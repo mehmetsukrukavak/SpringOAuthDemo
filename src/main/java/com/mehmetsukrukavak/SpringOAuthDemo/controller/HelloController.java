66f7f65fa1bd837f491e35a501644b62ecc168dc
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-02-23 17:15:27 +0300

    OAuth2 with Google

--- src/main/java/com/mehmetsukrukavak/SpringOAuthDemo/controller/HelloController.java
+++ src/main/java/com/mehmetsukrukavak/SpringOAuthDemo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.mehmetsukrukavak.SpringOAuthDemo;
+package com.mehmetsukrukavak.SpringOAuthDemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
